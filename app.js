0b9d1e45ef0903e7dd84ed0cd9e2e1bd27fe46e7
Grecy Jean <jeangrecy@gmail.com>
2023-01-21 12:08:32 -0500

    css validate updates

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const hamburger = document.querySelector(".hamburger");
+const hamburger = document.getElementById("hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
